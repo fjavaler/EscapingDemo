bdaba8f5ec263d790d2647e55431d7f6d462160d
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-03 19:40:28 -0600

    Initial Commit

---
+++ EscapingDemo/EscapingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EscapingDemoApp.swift
+//  EscapingDemo
+//
+//  Created by Fred Javalera on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct EscapingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
